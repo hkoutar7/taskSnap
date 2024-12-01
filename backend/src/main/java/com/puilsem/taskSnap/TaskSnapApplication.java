2f0a702aa41462326550b8afd19c31a51289b409
hkoutar7 <ayanokoji09opp@gmail.com>
2024-12-01 14:57:47 +0100

    Initializing the project

---
+++ backend/src/main/java/com/puilsem/taskSnap/TaskSnapApplication.java
@@ -0,0 +1,13 @@
+package com.puilsem.taskSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSnapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/puilsem/taskSnap/TaskSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puilsem.taskSnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
